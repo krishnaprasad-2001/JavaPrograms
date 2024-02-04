fd523bb403bf80337805e059d9b3a2fc655c01cd
krishnaprasad-2001 <krishnaprasadpa2001@gmail.com>
2024-02-04 22:28:12 +0530

    finished java game

--- SnakeGameJava/GamePanel.java
+++ SnakeGameJava/GamePanel.java
@@ -1,4 +1,6 @@
 import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.Color;
@@ -47,23 +49,35 @@ public class GamePanel extends JPanel implements ActionListener{
 
 	}
 	public void draw(Graphics g){
-		for(int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++){
-			g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-			g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+		if(running){
+
+			for(int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++){
+				g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+				g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+			}
+			g.setColor(Color.red);
+			g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+			for(int i=0;i<bodyParts;i++){
+				if(i==0){
+					g.setColor(Color.green);
+					g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+				}
+				else{
+
+					// g.setColor(new Color(45,180,0));
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+					g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+
+				}
+			}
+
+			g.setColor(Color.red);
+			g.setFont(new Font("Ink Free",Font.BOLD,40));
+			FontMetrics metrics=getFontMetrics(g.getFont());
+			g.drawString("score:"+applesEaten,(SCREEN_WIDTH-metrics.stringWidth("score:"+applesEaten))/2,g.getFont().getSize());
 		}
-		g.setColor(Color.red);
-		g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
-		for(int i=0;i<bodyParts;i++){
-			if(i==0){
-				g.setColor(Color.green);
-				g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
-			}
-			else{
-
-				g.setColor(new Color(45,180,0));
-				g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
-
-			}
+		else{
+			gameOver(g);
 		}
 
 	}
@@ -98,6 +112,12 @@ public class GamePanel extends JPanel implements ActionListener{
 
 	}
 	public void checkApple(){
+		if((x[0]==appleX) && (y[0]==appleY)){
+
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
 
 	}
 		// to check if head collides with
@@ -126,6 +146,15 @@ public class GamePanel extends JPanel implements ActionListener{
 
 	}
 	public void gameOver(Graphics g){
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free",Font.BOLD,75));
+		FontMetrics metrics=getFontMetrics(g.getFont());
+		g.drawString("Game over",(SCREEN_WIDTH-metrics.stringWidth("Game over"))/2,SCREEN_HEIGHT/2);
+
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free",Font.BOLD,40));
+		FontMetrics metrics1=getFontMetrics(g.getFont());
+		g.drawString("score:"+applesEaten,(SCREEN_WIDTH-metrics1.stringWidth("score:"+applesEaten))/2,g.getFont().getSize());
 
 	}
 	@Override
@@ -141,6 +170,34 @@ public class GamePanel extends JPanel implements ActionListener{
 	public class MyKeyAdapter extends KeyAdapter{
 		@Override 
 		public void keyPressed(KeyEvent e){
+			switch(e.getKeyCode()){
+				case KeyEvent.VK_LEFT:
+				if(direction!='R'){
+					direction='L';
+				}
+				break;
+			}
+			switch(e.getKeyCode()){
+				case KeyEvent.VK_RIGHT:
+				if(direction!='L'){
+					direction='R';
+				}
+				break;
+			}
+			switch(e.getKeyCode()){
+				case KeyEvent.VK_UP:
+				if(direction!='D'){
+					direction='U';
+				}
+				break;
+			}
+			switch(e.getKeyCode()){
+				case KeyEvent.VK_DOWN:
+				if(direction!='U'){
+					direction='D';
+				}
+				break;
+			}
 
 		}
 	}

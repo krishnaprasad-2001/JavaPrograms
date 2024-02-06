dda14bcb82caab06aaf741364393659d13b8361e
krishnaprasad-2001 <krishnaprasadpa2001@gmail.com>
2024-02-06 23:42:41 +0530

    Winding up in the date of 6 Feb with the very basics
    Reached till the page number 39 and have to reach till 100 tomorrow

---
+++ SimplePrograms/CharTest.java
@@ -0,0 +1,10 @@
+class CharTest{
+	public static void main(String args[]){
+		char ch1='X';
+		System.out.println("Ch1 is currently " + ch1);
+		ch1++;
+		System.out.println("Ch1 is currently " + ch1);
+	}
+}
+
+
